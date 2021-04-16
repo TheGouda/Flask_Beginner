9e9c851f8b8a5c4a993fbcd2600ac700e7162114
TheGouda <44629214+TheGouda@users.noreply.github.com>
2021-04-16 05:34:43 +0530

    prime code , sum code(not working)

--- main.py
+++ main.py
@@ -23,11 +23,38 @@ def armstrong(n):
         }
     return jsonify(res)
 
+
+
 @app.route('/hello/<string:s>')
 def hello(s):
     res={
         "Guest name":s
     }
+    return jsonify(res)
+
+@app.route('/add/a/<int:a>/b/<int:b>')
+def add(a,b):
+    c=a+b
+    res={
+        "First number":a , "Second number":b , "Sum":c
+    }
+
+@app.route('/prime/<int:n>')
+def prime(n):
+    f=False
+    if(n>1):
+        for i in range(2,n):
+            if(n%i)==0:
+                f=True
+                break
+        if(f):
+            res={
+                "Number":n,"Prime":False
+                }
+        else:
+            res={
+                "Number":n,"Prime":True
+                }
     return jsonify(res)
 
 if __name__ == "__main__":
